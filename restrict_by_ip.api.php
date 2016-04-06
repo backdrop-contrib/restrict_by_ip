0962ba525dc0ac0cb1798d2779384575f538d9e2
Brandon Williams <rocketeerbkw@gmail.com>
2016-04-05 19:14:19 -0500

    Issue #2691289 by rocketeerbkw: Allow client ip to be altered by other modules

---
+++ restrict_by_ip.api.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Documentation of restrict_by_ip hooks.
+ */
+
+/**
+ * Alter the IP address of the user.
+ *
+ * In some cases it may desirable to use alternative methods to determine the
+ * users IP address. This will alter the IP address that is compared with the
+ * configured IP whitelists.
+ *
+ * Warning: Using this hook has security implications. Providing a wrong IP
+ * address could allow users to bypass IP restrictions.
+ *
+ * @param $ip_address
+ *   IP address of user.
+ */
+function hook_restrict_by_ip_get_ip_alter(&$ip_address) {
+  // Use drupal core ip_address() instead.
+  $ip_address = ip_address();
+}
